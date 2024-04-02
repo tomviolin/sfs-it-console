ea46be89b19197d8d218fcb7d35a15e5d1e67aa4
Tom Hansen <tomh@uwm.edu>
2024-04-02 06:40:02 -0500

    server now runs in same directory as executable

--- server.sh
+++ server.sh
@@ -1,6 +1,9 @@
 #!/home/tomh/sfs-it-console/venv/bin/python3
-
+import os,sys
 from http.server import HTTPServer, CGIHTTPRequestHandler
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
 
 server_address = ("0.0.0.0", 8000)
 httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
